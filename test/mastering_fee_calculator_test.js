9853fd142587fe1550086e430b6890b057cf538c
Séverine Darlot <severine.darlot@gmail.com>
2013-01-09 21:57:23 +0100

    With EP and LP prices

--- test/mastering_fee_calculator_test.js
+++ test/mastering_fee_calculator_test.js
@@ -8,5 +8,13 @@ describe('Calculator', function(){
     it('should return 70 when there is 2 song', function(){
       /* 35 * 2 = 70 */
       assert.equal(70, calculator.compute(2));
-    })
+    });
+    it('should return 180 when there is 6 song', function(){
+      /* 1 EP is 6 songs and cost 180 */
+      assert.equal(180, calculator.compute(6));
+    });
+    it('should return 335 when there is 13 song', function(){
+      /* 1 LP is 13 songs and cost 335 */
+      assert.equal(335, calculator.compute(13));
+    });
 });
