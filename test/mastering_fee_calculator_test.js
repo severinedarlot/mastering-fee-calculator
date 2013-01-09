cf5016eacfe939f91f45307492d13a53294c6ed1
Séverine Darlot <severine.darlot@gmail.com>
2013-01-09 21:42:16 +0100

    2 basics songs

--- test/mastering_fee_calculator_test.js
+++ test/mastering_fee_calculator_test.js
@@ -1,8 +1,12 @@
 var assert = require("assert"),
   calculator = require("../lib/mastering_fee_calculator");
 
-describe('Array', function(){
-    it('should return 0 when there is 0 song', function(){
-      assert.equal(0, calculator.compute(0));
+describe('Calculator', function(){
+    it('should return 1 when there is 1 song', function(){
+      assert.equal(35, calculator.compute(1));
+    });
+    it('should return 70 when there is 2 song', function(){
+      /* 35 * 2 = 70 */
+      assert.equal(70, calculator.compute(2));
     })
 });
