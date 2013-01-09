68fdb7c178aeffb1e3d766700e4bc54854975c0e
Séverine Darlot <severine.darlot@gmail.com>
2013-01-09 22:13:42 +0100

    Fixe EP and LP prices at 30 euros and 26 euros

--- test/mastering_fee_calculator_test.js
+++ test/mastering_fee_calculator_test.js
@@ -9,12 +9,25 @@ describe('Calculator', function(){
       /* 35 * 2 = 70 */
       assert.equal(70, calculator.compute(2));
     });
-    it('should return 180 when there is 6 song', function(){
+    it('EP price: should return 180 when there is 6 song', function(){
       /* 1 EP is 6 songs and cost 180 */
       assert.equal(180, calculator.compute(6));
     });
-    it('should return 335 when there is 13 song', function(){
-      /* 1 LP is 13 songs and cost 335 */
-      assert.equal(335, calculator.compute(13));
+    it('EP price: should return 210 when there is 7 song', function(){
+      /* EP price : 180/6 * 7 */
+      assert.equal(210, calculator.compute(7));
     });
+    it('EP price: should return 360 when there is 12 song', function(){
+      /* EP price : 180/6 * 12 */
+      assert.equal(360, calculator.compute(12));
+    });
+    it('LP price: should return 335 when there is 13 song', function(){
+      /* 1 LP is 13 songs and cost 338 */
+      assert.equal(338, calculator.compute(13));
+    });
+     it('LP price: should return 361 when there is 14 song', function(){
+      /* LP price: 338/13 * 14 = 364 */
+      assert.equal(364, calculator.compute(14));
+    });
+
 });
