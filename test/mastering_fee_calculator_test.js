9909db0224bd863a93934fd2772fbca90e7f1ca3
Séverine Darlot <severine.darlot@gmail.com>
2013-01-09 23:02:53 +0100

    Change LP price to 360 euros

--- test/mastering_fee_calculator_test.js
+++ test/mastering_fee_calculator_test.js
@@ -2,36 +2,32 @@ var assert = require("assert"),
   calculator = require("../lib/mastering_fee_calculator");
 
 describe('Calculator', function(){
-    it('should return 1 when there is 1 song', function(){
+    it('should return 35 when there is 1 song', function(){
       assert.equal(35, calculator.compute(1));
     });
-    it('should return 70 when there is 2 song', function(){
-      /* 35 * 2 = 70 */
+    it('should return 35 * 2 = 70 when there is 2 songs', function(){
       assert.equal(70, calculator.compute(2));
     });
-    it('EP price: should return 180 when there is 6 song', function(){
-      /* 1 EP is 6 songs and cost 180 */
+    it('1 EP is 6 songs and cost 180', function(){
       assert.equal(180, calculator.compute(6));
     });
-    it('EP price: should return 210 when there is 7 song', function(){
-      /* EP price : 180/6 * 7 */
+    it('EP price: should return 180/6 * 7 = 210 when there is 7 songs', function(){
       assert.equal(210, calculator.compute(7));
     });
-    it('EP price: should return 330 when there is 11 song', function(){
-      /* EP price : 180/6 * 11 = 330 */
+    it('EP price: should return 180/6 * 11 = 330 when there is 11 songs', function(){
       assert.equal(330, calculator.compute(11));
     });
-    it('EP price: should return 360 when there is 12 song', function(){
-      /* EP price : 180/6 * 12 = 360 */
+    it('EP price: should return 180/6 * 12 = 360 when there is 12 songs', function(){
       assert.equal(360, calculator.compute(12));
     });
-    it('LP price: should return 335 when there is 13 song', function(){
-      /* 1 LP is 13 songs and cost 335 */
-      assert.equal(335, calculator.compute(13));
+    it('LP priceis 13 songs and cost 360', function(){
+      assert.equal(360, calculator.compute(13));
     });
-     it('LP price: should return 360.77 when there is 14 song', function(){
-      /* LP price: 335/13 + 335 = 360.77 */
-      assert.equal(360.77, calculator.compute(14));
+    it('LP price: should return 360/13 + 360 = 360.77 when there is 14 songs', function(){
+      assert.equal(387.70, calculator.compute(14));
+    });
+    it('LP price: should return 360/13*7 + 360 = 515.39 when there is 20 songs', function(){
+      assert.equal(553.85, calculator.compute(20));
     });
 
 });
