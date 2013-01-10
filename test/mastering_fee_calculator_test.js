da6c5802bd784ef3ba1a50c7a154b9284fa51030
Séverine Darlot <severine.darlot@gmail.com>
2013-01-10 13:18:57 +0100

    Prepare new data structure

--- test/mastering_fee_calculator_test.js
+++ test/mastering_fee_calculator_test.js
@@ -3,31 +3,48 @@ var assert = require("assert"),
 
 describe('Calculator', function(){
     it('should return 35 when there is 1 song', function(){
-      assert.equal(35, calculator.compute(1));
+      var inputs = new function () { this.songCount = 1};
+      assert.equal(35, calculator.compute(inputs));
     });
     it('should return 35 * 2 = 70 when there is 2 songs', function(){
-      assert.equal(70, calculator.compute(2));
+      var inputs = new function () { this.songCount = 2};
+      assert.equal(70, calculator.compute(inputs));
     });
     it('1 EP is 6 songs and cost 180', function(){
-      assert.equal(180, calculator.compute(6));
+      var inputs = new function () { this.songCount = 6};
+      assert.equal(180, calculator.compute(inputs));
     });
     it('EP price: should return 180/6 * 7 = 210 when there is 7 songs', function(){
-      assert.equal(210, calculator.compute(7));
+      var inputs = new function () { this.songCount = 7};
+      assert.equal(210, calculator.compute(inputs));
     });
     it('EP price: should return 180/6 * 11 = 330 when there is 11 songs', function(){
-      assert.equal(330, calculator.compute(11));
+      var inputs = new function () { this.songCount = 11};
+      assert.equal(330, calculator.compute(inputs));
     });
     it('EP price: should return 180/6 * 12 = 360 when there is 12 songs', function(){
-      assert.equal(360, calculator.compute(12));
+      var inputs = new function () { this.songCount = 12};
+      assert.equal(360, calculator.compute(inputs));
     });
     it('LP priceis 13 songs and cost 360', function(){
-      assert.equal(360, calculator.compute(13));
+      var inputs = new function () { this.songCount = 13};
+      assert.equal(360, calculator.compute(inputs));
     });
     it('LP price: should return 360/13 + 360 = 360.77 when there is 14 songs', function(){
-      assert.equal(387.70, calculator.compute(14));
+      var inputs = new function () { this.songCount = 14};
+      assert.equal(387.70, calculator.compute(inputs));
     });
     it('LP price: should return 360/13*7 + 360 = 515.39 when there is 20 songs', function(){
-      assert.equal(553.85, calculator.compute(20));
+      var inputs = new function () { this.songCount = 20};
+      assert.equal(553.85, calculator.compute(inputs));
     });
+    /*it('with stem mastering', function () {
+      var inputs = new function () {
+        this.stem = true;
+        this.songCount = 1;
+      };
+      console.log(inputs);
+      assert.equal(12 + 35, calculator.compute(inputs));
+    });*/
 
 });
