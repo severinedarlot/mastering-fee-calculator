3fb330363867456d75bb50730518a7c123773e59
Séverine Darlot <severine.darlot@gmail.com>
2013-01-29 22:37:37 +0100

    Stem added

--- js/app.js
+++ js/app.js
@@ -4,13 +4,32 @@ App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
   templateName: 'application',
   title: 'Calculator',
-  songCounts: [1, 2, 3, 4],
   songCountSelected: 0,
+  stem : false,
+
   result: function () {
     var self = this,
-      inputs = new function () { this.songCount = {0: self.get('songCountSelected')}};
+      inputs = new function () {
+        this.songCount = {0: self.get('songCountSelected')};
+        this.stem = self.get('stem');
+        this.extraLoud = true;
+        this.ddpCoding = true;
+        this.pressDelivery = true;
+        this.pboInstru = 5;
+        this.postalSending = true;
+      };
     return compute(inputs);
-  }.property('songCountSelected')
+  }.property('songCountSelected', 'stem'),
+
+  songCounts: function () {
+    var result = [];
+    for(var i=1 ; i < 25 ; i++) {
+      result.pushObject(i);
+    }
+    return result;
+  }.property(),
+
+
 });
 
 App.initialize();
