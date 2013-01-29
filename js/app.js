aff9eac6e8704ffbcd3dcba4a32c6af733ae3c9d
Séverine Darlot <severine.darlot@gmail.com>
2013-01-29 23:22:10 +0100

    Add others checkboxes

--- js/app.js
+++ js/app.js
@@ -6,20 +6,24 @@ App.ApplicationView = Ember.View.extend({
   title: 'Calculator',
   songCountSelected: 0,
   stem : false,
+  extraLoud : false,
+  ddpCoding : false,
+  pressDelivery : false,
+  postalSending : false,
 
   result: function () {
     var self = this,
       inputs = new function () {
         this.songCount = {0: self.get('songCountSelected')};
         this.stem = self.get('stem');
-        this.extraLoud = true;
-        this.ddpCoding = true;
-        this.pressDelivery = true;
+        this.extraLoud = self.get('extraLoud');
+        this.ddpCoding = self.get('ddpCoding');
+        this.pressDelivery = self.get('pressDelivery');
         this.pboInstru = 5;
-        this.postalSending = true;
+        this.postalSending = self.get('postalSending');
       };
     return compute(inputs);
-  }.property('songCountSelected', 'stem'),
+  }.property('songCountSelected', 'stem', 'extraLoud', 'ddpCoding', 'pressDelivery', 'postalSending'),
 
   songCounts: function () {
     var result = [];
