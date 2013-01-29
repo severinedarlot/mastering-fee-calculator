e56aabc77a0cc607ad6cd5d2cc998876305fda81
Séverine Darlot <severine.darlot@gmail.com>
2013-01-29 22:03:00 +0100

    First usage of compute

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@ var App = Ember.Application.create();
 App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
   templateName: 'application',
-  title: 'Calculator'
+  title: 'Calculator',
+  songCounts: [1, 2, 3, 4],
+  songCountSelected: 0,
+  result: function () {
+    var self = this,
+      inputs = new function () { this.songCount = {0: self.get('songCountSelected')}};
+    return compute(inputs);
+  }.property('songCountSelected')
 });
 
 App.initialize();
