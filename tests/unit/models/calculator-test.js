853658fac62dfd80af1ca9f32733b7d3e0e35919
Séverine Darlot <severine.darlot@gmail.com>
2014-08-04 22:58:03 +0200

    Tests refactored to be more readable

--- tests/unit/models/calculator-test.js
+++ tests/unit/models/calculator-test.js
@@ -1,34 +1,39 @@
 import Ember from 'ember';
 import Calculator from '../../../models/calculator';
 
+
 test('total count  = firstSliceCount + secondSliceCount + thirdSliceCount + fourthSliceCount', function () {
-	var calculator;
-	Ember.run(function() {
-		calculator = Calculator.create({
-		  firstSliceCount: 4,
-      secondSliceCount: 2,
-      thirdSliceCount: 1,
-      fourthSliceCount: 3
-	  });
+	var calculator = calculatorFactory({
+	  firstSliceCount: 4,
+    secondSliceCount: 2,
+    thirdSliceCount: 1,
+    fourthSliceCount: 3
 	});
   equal(calculator.get('totalCount'), 10);
 });
 
 test('When nothing, fee prices are zero', function () {
-	var calculator;
-	Ember.run(function() {
-		calculator = Calculator.create();
-	});
-	equal(calculator.get('titlesPrice'), 0);
-  equal(calculator.get('htPrice'), 0);
+	var calculator = calculatorFactory({});
+  equal(calculator.get('titlesPrice'), 0);
+	equal(calculator.get('htPrice'), 0);
 });
 
 test('titlesPrice should return 48 when there is 1 song of first slice', function () {
-	var calculator;
-	Ember.run(function() {
-		calculator = Calculator.create({
-		  firstSliceCount: 1
-	  });
-	});
+	var calculator = calculatorFactory({
+    firstSliceCount: 1
+  });
 	equal(calculator.get('titlesPrice'), 48);
 });
+
+test('titlesPrice should return 48 * 2 = 96 when there is 2 songs', function () {
+  var calculator = calculatorFactory({firstSliceCount: 2});
+  equal(calculator.get('titlesPrice'), 96);
+});
+
+function calculatorFactory(input) {
+  var calc;
+	Ember.run(function() {
+		calc = Calculator.create(input);
+	});
+	return calc;
+}
